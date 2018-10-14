b3567dead0442755f1e79474b93bee75333db5e7
Krystian Sitarek <krystiansitarek@gmail.com>
2018-10-14 11:57:20 +0200

    Init vtb & vtb.Api projects

---
+++ vtb.Api/Controllers/VtbController.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Distributed;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace vtb.Api.Controllers
+{
+    [ApiController]
+    public abstract class VtbController : ControllerBase
+    {
+    }
+}
--- vtb.Api/Startup.cs
+++ vtb.Api/Startup.cs
@@ -1,20 +1,45 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
+using AutoMapper;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Swashbuckle.AspNetCore.Swagger;
+using vtb.Warehouse.Data;
 
 namespace vtb.Api
 {
     public class Startup
     {
+        private readonly IConfiguration _config;
+
+        public Startup(IConfiguration configuration)
+        {
+            _config = configuration;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddWarehouses(_config);
+            services.AddDistributedRedisCache(o =>
+            {
+                o.Configuration = "127.0.0.1";
+                o.InstanceName = "master";
+            });
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "API DOC", Version = "v1" });
+            });
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -25,10 +50,14 @@ namespace vtb.Api
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) =>
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                c.RoutePrefix = "api-doc";
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
             });
+
+            app.UseMvcWithDefaultRoute();
         }
     }
 }
